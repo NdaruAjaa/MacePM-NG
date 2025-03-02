45d635aa1c97a5ea5fbbc90127ce11e9845d294b
NdaruAjaa <120300909+NdaruAjaa@users.noreply.github.com>
2025-03-02 15:45:52 +0700

    NG Support

--- src/particle/WindParticle.php
+++ src/particle/WindParticle.php
@@ -4,13 +4,15 @@ namespace XeonCh\Mace\particle;
 
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\network\mcpe\protocol\types\ParticleIds;
 use pocketmine\world\particle\Particle;
 
-class WindParticle implements Particle
-{
-    public function encode(Vector3 $pos): array
-    {
-        return [LevelEventPacket::standardParticle(91, 0, $pos)];
+class WindParticle implements Particle {
+
+    public function encode(Vector3 $pos): array {
+        return [
+            LevelEventPacket::standardParticle(ParticleIds::WIND_EXPLOSION, 0, $pos, ProtocolInfo::CURRENT_PROTOCOL)
+        ];
     }
 }
